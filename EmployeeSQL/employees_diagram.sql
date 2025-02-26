a46bcf65920f225d10fb8b077541fd6e7a5849cc
“M0ther0fGingers” <jen@robisons.us>
2025-02-25 20:13:03 -0600

    Updated the database diagram

--- EmployeeSQL/employees_diagram.sql
+++ EmployeeSQL/employees_diagram.sql
@@ -43,10 +43,13 @@ CREATE TABLE "titles" (
      )
 );
 
-CREATE TABLE "saleries" (
-    "emp_no" VARCHAR   NOT NULL,
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
     "salary" int   NOT NULL
 );
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
@@ -60,9 +63,6 @@ REFERENCES "departments" ("dept_no");
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
-REFERENCES "employees" ("emp_title_id");
-
-ALTER TABLE "saleries" ADD CONSTRAINT "fk_saleries_emp_no" FOREIGN KEY("emp_no")
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
